bea7c87ce95d256239e2bf8c1c59dcf862c55845
James Blacklock <jamesblacklock@protonmail.com>
2022-05-12 13:36:32 -0400

    add stuff

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fortunes = require('./fortunes');
+const Stuff = require('./stuff-model');
 
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('<h1>HEROKU DEPLOYMENT!!</h1>');
@@ -15,5 +18,33 @@ server.get('/fortune', (req, res) => {
     let randomIndex = Math.floor(Math.random() * fortunes.length);
     res.send(`<h1>${fortunes[randomIndex]}</h1>`);
 });
+
+
+
+server.get('/stuff', (req, res) => {
+    Stuff.getAll()
+        .then(result => {
+            res.json(result);
+        });
+});
+
+server.get('/stuff/:index', (req, res) => {
+    Stuff.getByIndex(req.params.index)
+        .then(result => {
+            if(result == null) {
+                res.status(404).json({ message: 'not found' });
+                return;
+            }
+            res.json(result);
+        });
+});
+
+server.post('/stuff', (req, res) => {
+    Stuff.add(req.body)
+        .then(result => {
+            res.status(201).json(result);
+        });
+});
+
 
 module.exports = server;
